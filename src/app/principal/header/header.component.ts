750cbf7aa796338a233bf213156c29410d5a2f74
Juliana Rendon Ruiz <1412075@SOAM.TCS.com>
2018-08-06 17:17:14 -0500

    commit14

--- src/app/principal/header/header.component.ts
+++ src/app/principal/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-header',
@@ -7,23 +6,12 @@ import { ObservableMedia, MediaChange } from '@angular/flex-layout';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public watcher: any;
-  public opened: boolean;
-  public over: string;
 
   public ngOnInit() {
 
   }
 
-  constructor(public media: ObservableMedia) {
-    this.watcher = media.subscribe((change: MediaChange) => {
-      if (change.mqAlias === 'sm' || change.mqAlias === 'xs') {
-        this.opened = false;
-        this.over = 'over';
-      } else {
-        this.opened = true;
-        this.over = 'side';
-      }
-    });
+  constructor() {
+
   }
 }
